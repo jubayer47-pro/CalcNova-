fbb03827730aafdb242d57fe90723175248d3f7b
jubayer47-pro <prodhanjubayer47@gmail.com>
2026-08-16 02:06:26 +0600

    Initialize build.gradle.kts for Android app

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application") version "8.7.3"
+    id("org.jetbrains.kotlin.android") version "2.0.21"
+}
+
+android {
+    namespace = "com.calcnova.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.calcnova.app"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("com.google.android.gms:play-services-ads:24.5.0")
+}
